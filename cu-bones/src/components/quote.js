f53a8dba6f673f3f6bf95836728acb75c26cc377
Sydney Lawrence <sll224@cornell.edu>
2020-06-29 09:54:17 -0400

    Add basic quote design

--- cu-bones/src/components/quote.js
+++ cu-bones/src/components/quote.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card'
+import Text from 'react';
+
+import '../css/site.css'
 
 class Quote extends React.Component {
 
   constructor(props) {
     super(props);
-    // TODO add state if needed
   }
 
   render() {
     // TODO implement
     return (
-      <div>
-        <p>Quote</p>
-      </div>
+      <Card>
+        <Card.Header></Card.Header>
+        <Card.Body>
+          <blockquote className="blockquote mb-0 ">
+            <p className="newlineable">
+              {this.props.children}
+            </p>
+            <footer className="blockquote-footer">
+              {this.props.author}
+            </footer>
+          </blockquote>
+        </Card.Body>
+      </Card>
     );
   }
 }
--- cu-bones/src/pages/Quotes.js
+++ cu-bones/src/pages/Quotes.js
@@ -5,7 +5,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import SideNavbar from '../components/navbar.js'
-
+import Quote from '../components/quote.js'
 import '../css/site.css';
 
 
@@ -17,9 +17,23 @@ function Quotes() {
         <SideNavbar />
         <Col class="blue">
           <Header title="Quotes"
-            desc="A wise bone once said..." />
+            desc="A wise bone(head) once said..." />
           <article>
-            <p>Nothing to see here yet...</p>
+            <Quote author="Brian">
+              Ayy, that's the... uhh...
+              <br /><br />
+              Fish!
+              </Quote>
+            <br />
+            <Quote author="Brian">
+              Who here knows about the Wario apparition?
+            </Quote>
+            <br />
+            <Quote author="also Brian">
+              Ah yes, my lower intestinal tract is just-<br />
+              *hums minecraft music*<br />
+              *immitates breaking blocks in minecraft and popping the items into his inventory*
+            </Quote>
           </article>
         </Col>
       </Row>
